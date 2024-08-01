2ba2694d861a82e4cf06dbf03e839c11d5cd6ce7
sberdzik-apptio <sberdzik@apptio.com>
2024-08-01 18:00:26 +0200

    fix feign client request scope limit

--- src/main/java/com/oxygensend/commonspring/feign/CommonFeignConfiguration.java
+++ src/main/java/com/oxygensend/commonspring/feign/CommonFeignConfiguration.java
@@ -1,15 +1,17 @@
 package com.oxygensend.commonspring.feign;
 
 import com.oxygensend.commonspring.request_context.RequestContext;
-import feign.Client;
-import feign.RequestInterceptor;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.slf4j.MDC;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import feign.Client;
+import feign.RequestInterceptor;
 
 @Configuration
 @EnableConfigurationProperties(FeignConnectionProperties.class)
@@ -30,15 +32,16 @@ public class CommonFeignConfiguration {
     Client uriFeignClient(FeignConnectionProperties properties, FeignClientFactory feignClientFactory,
                           LoadBalancerClient loadBalancerClient, LoadBalancerClientFactory loadBalancerClientFactory) {
         return switch (properties.type()) {
-            case SERVICE_DISCOVERY -> feignClientFactory.eurekaFeignClient(loadBalancerClient, loadBalancerClientFactory);
+            case SERVICE_DISCOVERY ->
+                feignClientFactory.eurekaFeignClient(loadBalancerClient, loadBalancerClientFactory);
             case URI -> feignClientFactory.uriFeignClient();
         };
     }
 
     @Bean
-    RequestInterceptor requestIdHeaderPropagation(RequestContext requestContext) {
+    RequestInterceptor requestIdHeaderPropagation() {
         return requestTemplate -> {
-            requestTemplate.header("Request-Id", requestContext.requestId());
+            requestTemplate.header("Request-Id", MDC.get("Request-Id"));
         };
     }
 
